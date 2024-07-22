a20b6e7698154311c1e7f3606d2f41488b0263ec
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-07-22 23:11:41 +0700

    api

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
-
+"use client"
 import { Nav } from "@/components/navbar/nav";
-import { ModeState } from "@/context/switchthem";
 import { Homepage } from "@/components/home/home";
+import { useEffect, useState } from "react";
+
 
 export default function Home() {
+  const [imageURL, setImageURL] = useState<string[] | undefined>()
+  useEffect(()=>{
+    const fetchImage = async () => {
+      try {
+        const res = await fetch('/api/getskills')
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        const data = await res.json()
+        setImageURL(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchImage()
+  },[])
+  useEffect(()=>{
+    console.log(imageURL)
+  },[imageURL])
   return (
     <>
       <Nav/>
       <Homepage/>
+      
     </>
   );
 }
--- components/home/home.tsx
+++ components/home/home.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Hero } from "./hero";
 import { Word } from "./word";
 
