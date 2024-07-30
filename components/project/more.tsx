419170155d90075be4e201354adda5a70c72ca3a
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-07-30 22:38:14 +0700

    projects done

---
+++ components/project/more.tsx
@@ -0,0 +1,96 @@
+import { motion, useMotionValue } from "framer-motion";
+import { IoClose } from "react-icons/io5";
+import { FaClosedCaptioning, FaGithub } from "react-icons/fa6";
+import { FiExternalLink } from "react-icons/fi";
+import Image from "next/image";
+import Link from "next/link";
+import { DocumentData } from "firebase/firestore";
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+const DRAG_BUFFER = 50
+
+interface Props {
+    isSelected : DocumentData,
+    setSelected : Dispatch<SetStateAction<DocumentData>>|any
+}
+
+export const More = ({isSelected, setSelected} : Props) => {
+    const [dragging, setDragging] = useState(false)
+    const [imgIdx, setImgIdx] = useState(0)
+    const dragX = useMotionValue(0)
+    const dragEnd = () => {
+        setDragging(true)
+        const x = dragX.get()
+        if (x <= -DRAG_BUFFER && imgIdx < isSelected.images.length - 1) {
+            setImgIdx(prev => prev + 1)
+        } else if (x >= DRAG_BUFFER && imgIdx > 0) {
+            setImgIdx(prev => prev - 1)
+        }
+    }
+    return (
+        <div className="fixed inset-0 z-40 backdrop-blur-lg p-6 overflow-y-scroll">
+            <div className="text-end">
+                <button onClick={()=>setSelected(null)}><IoClose size={25}/></button>
+            </div>
+            <section className="max-w-3xl m-auto text-base overflow-hidden rounded-2xl">
+                <motion.div 
+                    className="flex cursor-grab active:cursor-grabbing" 
+                    dragConstraints={{ left : 0, right : 0 }} 
+                    drag="x"
+                    onDragStart={()=>setDragging(true)}
+                    onDragEnd={dragEnd}
+                    style={{ x : dragX }}
+                    animate={{ translateX : `-${imgIdx*100}%` }}>
+                        {isSelected.images.map((url:string, idx:number) => (
+                            <div key={idx} className="shrink-0 w-full relative">
+                                <Image
+                                    src={url}
+                                    alt={isSelected.name}
+                                    width={2000}
+                                    height={2000}
+                                    className="w-auto h-auto"
+                                />
+                                <div className="absolute inset-0"></div>
+                            </div>
+                        ))}
+                </motion.div>
+                <motion.div
+                    initial={{ y : "200%" }}
+                    animate={{ y : 0 }}
+                    exit={{ y : "200%" }}
+                    transition={{ duration : 0.5 }}
+                    className="bg-gray-50 dark:bg-neutral-700 p-6">
+                    <div className="flex gap-x-2 w-fit mx-auto mb-6">
+                        {isSelected.images.map((img:string, idx:number) => (
+                            <button 
+                                key={idx} 
+                                onClick={()=>setImgIdx(idx)} 
+                                className={`${imgIdx === idx ? "dark:bg-neutral-200 bg-gray-700" : "dark:bg-neutral-500 bg-gray-400"} w-2 h-2 rounded-full`}/>
+                        ))}
+                    </div>
+                    <h1 className="text-3xl font-black gradient-text">{isSelected.name}</h1>
+                    {isSelected.description.map((par : string, idx:number)=>(
+                        <p key={idx} className="text-justify my-2">{par}</p>
+                    ))}
+                    <h2 className="text-2xl font-bold gradient-text mb-1 mt-6">Features</h2>
+                    <ul>
+                        {isSelected.features.map((feature : string, idx:string)=>(
+                            <li key={idx}>{feature}</li>
+                        ))}
+                    </ul>
+                    <h2 className="text-2xl font-bold gradient-text mb-2 mt-6">Project Links</h2>
+                    <div className="flex gap-2">
+                        <Link href={isSelected.github} className="flex gap-1 items-center">
+                            <FaGithub size={20}/>
+                            <p>Source Code</p>
+                        </Link>
+                        <Link href={isSelected.link} className="flex gap-1 items-center">
+                            <FiExternalLink size={20}/>
+                            <p>Live Project</p>
+                        </Link>
+                    </div>
+                </motion.div>
+            </section>
+        </div>
+    )
+};
--- components/project/project.tsx
+++ components/project/project.tsx
@@ -1,44 +1,71 @@
 import Image from "next/image";
+import Link from "next/link";
 import { useState, useEffect } from "react";
+import { FaGithub } from "react-icons/fa6";
+import { FiExternalLink } from "react-icons/fi";
+import { DocumentData } from "firebase/firestore";
+import { AnimatePresence, motion } from "framer-motion";
+import { More } from "./more";
 
 export const Project = () => {
-    const [project, setProject] = useState<string[] | undefined>()
+    const [projects, setProjects] = useState<DocumentData[]>()
+    const [isSelected, setSelected] = useState<DocumentData>()
     useEffect(()=>{
-        const fetchProject = async () => {
+        const fetchProjects = async () => {
         try {
             const res = await fetch('/api/getproject')
             if (!res.ok) {
             throw new Error(`HTTP error! status: ${res.status}`);
             }
             const data = await res.json()
-            setProject(data)
+            setProjects(data)
         } catch (error) {
             console.error(error)
         }
         }
-        fetchProject()
+        fetchProjects()
     },[])
-    useEffect(()=>{
-        console.log(project)
-    },[project])
     return (
         <section id="project" className="">
             <div className="flex mb-4 gap-4">
                 <div className="h-[1px] self-center bg-gradient-to-l from-purple-600 to-pink-600  w-full"></div>
-                <h2 className="font-black text-5xl gradient-text">Project</h2>
+                <h2 className="font-black text-5xl gradient-text">Projects</h2>
             </div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-12">
-                <div className="bg-black h-[15rem]">
-                    <Image
-                        src="https://firebasestorage.googleapis.com/v0/b/portfolio-2b753.appspot.com/o/SEA%20Salon%2Fsea-salon.png?alt=media&token=37d65e24-dbd7-4438-bff4-6759d6a3dee6"
-                        width={1000}
-                        height={1000}
-                        alt="image"
-                        className="w-full h-full"
-                    />
-                </div>
-                <div className="bg-black h-[15rem]">tes</div>
-                <div className="bg-black h-[15rem]">tes</div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-12 text-lg">
+                {projects?.map(project=>(
+                    <div key={project.name}>
+                        <button onClick={()=>setSelected(project)} className="bg-gradient h-[15rem] rounded-xl mb-6 w-full">
+                            <motion.div className="w-full h-full">    
+                                <Image
+                                    src={project.images[0]}
+                                    alt={project.name}
+                                    width={700}
+                                    height={700} 
+                                    className="h-full scale-75 origin-bottom"
+                                />
+                            </motion.div>
+                        </button>
+                        <div className="flex gap-2 items-center">
+                            <h2 className="text-nowrap text-xl gradient-text font-bold">{project.name}</h2>
+                            <div className="h-[1px] w-full bg-gradient"/>
+                            <Link href={project.github}>
+                                <FaGithub size={25}/>
+                            </Link>
+                            <Link href={project.link}>
+                                <FiExternalLink size={25}/>
+                            </Link>
+                        </div>
+                        <ul className="flex gap-2 my-3 flex-wrap">
+                            {project.tech.map((tech:string, idx:number)=>(
+                                <li key={idx} className="px-2 bg-gray-200 dark:bg-neutral-700 rounded-full">{tech}</li>
+                            ))}
+                        </ul>
+                        <p>{project.description[0]}</p>
+                    </div>
+                ))}
+                <AnimatePresence>
+                    {isSelected && (<More isSelected={isSelected} setSelected={setSelected}/>)}
+                </AnimatePresence>
             </div>
         </section>
     )
