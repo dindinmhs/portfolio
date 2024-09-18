09c5d13333b5a0457a297bdbc4ed91bde77e1aa3
Dindin Imanudin <dindin.imanudin@mhs.itenas.ac.id>
2024-09-18 11:08:15 +0700

    fixed bug

--- components/home/word.tsx
+++ components/home/word.tsx
@@ -1,67 +1,81 @@
-"use client"
+"use client";
 import { useEffect, useRef, useState } from "react";
 import { Ubuntu_Mono } from "next/font/google";
 
-const ubuntu_mono = Ubuntu_Mono({ subsets: ["latin"], weight : "700" });
+const ubuntu_mono = Ubuntu_Mono({ subsets: ["latin"], weight: "700" });
 
 export const Word = () => {
-    let jobIdx = 0
-    const job = ["Student", "Programmer", "Developer", "Engineer"]
-    const charRandom = "!@#$%^&*():{};|,.<>/?"
-    const intervalRef = useRef<any>(null)
-    const [ number, setNumber ] = useState(0)
-    const [ text, setText ] = useState(job[jobIdx])
+    const jobIdxRef = useRef(0); 
+    const job = ["Student", "Programmer", "Developer", "Engineer"];
+    const charRandom = "!@#$%^&*():{};|,.<>/?";
+    const intervalRef = useRef<any>(null);
+    const [number, setNumber] = useState(0);
+    const [text, setText] = useState(job[jobIdxRef.current]);
 
     const scramble = () => {
-        let pos = 0
+        let pos = 0;
         intervalRef.current = setInterval(() => {
-            const scrambled = job[jobIdx].split("")
-              .map((char, index) => {
-                if (pos / 2 > index) {
-                  return char;
-                }
-      
-                const randomCharIndex = Math.floor(Math.random() * charRandom.length);
-                const randomChar = charRandom[randomCharIndex];
-      
-                return randomChar;
-              })
-              .join("");
+            const scrambled = job[jobIdxRef.current].split("")
+                .map((char, index) => {
+                    if (pos / 2 > index) {
+                        return char;
+                    }
 
-              setText(scrambled);
-              pos++;
-        
-              if (pos >= job[jobIdx].length * 4) {
-                jobIdx++
-                if (jobIdx >= job.length) {
-                    jobIdx = 0
+                    const randomCharIndex = Math.floor(Math.random() * charRandom.length);
+                    const randomChar = charRandom[randomCharIndex];
+
+                    return randomChar;
+                })
+                .join("");
+
+            setText(scrambled);
+            pos++;
+
+            if (pos >= job[jobIdxRef.current].length * 4) {
+                jobIdxRef.current++;
+                if (jobIdxRef.current >= job.length) {
+                    jobIdxRef.current = 0;
                 }
-                setNumber(jobIdx)
-                pos = 0
+                setNumber(jobIdxRef.current);
+                pos = 0;
                 stopScramble();
-              } 
-            },100)
-    }
+            }
+        }, 100);
+    };
 
     const stopScramble = () => {
-        console.log('stop scrumble')
         clearInterval(intervalRef.current || undefined);
-        setText(text);
-      };
-    useEffect(()=>{
-        console.log('start scrumble')
-        scramble()
-    },[])
+        setText(job[jobIdxRef.current]); 
+    };
+
+    useEffect(() => {
+        console.log("start scramble");
+        scramble();
+    }, []);
+
     return (
         <div className="text-2xl md:text-4xl">
             <h3>Hi i&apos;m</h3>
-            <h2 className="text-4xl md:text-6xl font-black bg-gradient-to-l from-purple-600 to-pink-600 mask">Dindin Imanudin</h2>
-            <h3>I&apos;m a <span className={`${ubuntu_mono.className} ${
-                    number === 0 ? "text-purple-500" :
-                    number === 1 ? "text-pink-500" :
-                    number === 2 ? "text-green-500" :
-                    "text-sky-500" 
-                }`}>{text}</span></h3>
+            <h2 className="text-4xl md:text-6xl font-black bg-gradient-to-l from-purple-600 to-pink-600 mask">
+                Dindin Imanudin
+            </h2>
+            <h3>
+                I&apos;m a{" "}
+                <span
+                    className={`${ubuntu_mono.className} ${
+                        number === 0
+                            ? "text-purple-500"
+                            : number === 1
+                            ? "text-pink-500"
+                            : number === 2
+                            ? "text-green-500"
+                            : "text-sky-500"
+                    }`}
+                >
+                    {text}
+                </span>
+            </h3>
         </div>
-    )
+    );
 };
+
